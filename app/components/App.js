47c88888350d01984c301f576849006930b589ce
mdwareing <mdwareing@gmail.com>
2018-08-27 11:29:26 +0100

    Add Form

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React from 'react'
 import Popular from './Popular'
+const ReactRouter = require('react-router-dom')
+import Home from './Home'
+import Nav from './Nav'
+import Battle from './Battle'
+
+const Router = ReactRouter.BrowserRouter
+const Route = ReactRouter.Route
+const Switch = ReactRouter.Switch
 
 export default class App extends React.Component {
     render() {
         return (
-            <div className="container">
-                <Popular />
-            </div>
+            <Router>
+                <div className="container">
+                    <Nav />
+                    <Switch>
+                    <Route exact path='/' component={Home} />
+                    <Route exact path ='/battle' component={Battle} />
+                    <Route path='/popular' component={Popular} />
+                    <Route render={function() {
+                        return <p>Not Found!</p>
+                    }} />
+                    </Switch>
+                </div>
+            </Router>
         )
     }
 }
